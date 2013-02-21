ddf5725fd8c53a75a273071a9c639706f599d8ff
elainebx <elaine@factual.com>
2013-02-21 16:47:43 +0800

    fixed issue with load previous key

--- build/options.js
+++ build/options.js
@@ -1,21 +1,21 @@
-function upload_options() {
-  var table_key = document.getElementById("key_value");
+function save_options() {
+  var table_key = document.getElementById("key_value").value;
   localStorage["last_key"] = table_key;
-  var secret_value = document.getElementById("secret_value");
+  var secret_value = document.getElementById("secret_value").value;
   localStorage["last_secret"] = secret_value;
-  alert("Submitted.")
+  alert("Submitted " + localStorage["last_key"])
 };
 
 function restore_options() {
   var previous_key = localStorage["last_key"];
-  if (!previous_key) {
-    document.getElementById("key_value").innerHTML = previous_key;
-  }
   var last_secret = localStorage["last_secret"];
-  if (!last_secret) {
-    document.getElementById("secret_value").innerHTML = last_secret;
-  }
+  if (previous_key) {
+    document.getElementById("key_value").value = previous_key;
+  };
+  if (last_secret) {
+  document.getElementById("secret_value").value = last_secret;
+  };
 };
 
+document.querySelector('#save').addEventListener('click', save_options);
 document.addEventListener('DOMContentLoaded', restore_options);
-document.querySelector('.btn').addEventListener('click', save_options);
