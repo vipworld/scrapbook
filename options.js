a1e65426aa68c69c95602885f12fdc99b3222d47
elainebx <elaine@factual.com>
2013-02-21 12:26:12 +0800

    updated options html and js

---
+++ options.js
@@ -0,0 +1,7 @@
+$(function upload_options() {
+  var table_key = document.getElementById("key_value");
+  
+  var secret_value = document.getElementById("secret_value");
+
+  alert("Submitted Succefully.")
+});
