d18e7a793173efea25a43a09d21da264a1ac4dd7
elainebx <elaine@factual.com>
2013-02-21 15:06:55 +0800

    updated chrome storage to memorize last key used

--- build/options.js
+++ build/options.js
@@ -1,7 +1,21 @@
-$(function upload_options() {
+function upload_options() {
   var table_key = document.getElementById("key_value");
-  
+  localStorage["last_key"] = table_key;
   var secret_value = document.getElementById("secret_value");
+  localStorage["last_secret"] = secret_value;
+  alert("Submitted.")
+};
 
-  alert("Submitted Succefully.")
-});
+function restore_options() {
+  var previous_key = localStorage["last_key"];
+  if (!previous_key) {
+    document.getElementById("key_value").innerHTML = previous_key;
+  }
+  var last_secret = localStorage["last_secret"];
+  if (!last_secret) {
+    document.getElementById("secret_value").innerHTML = last_secret;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.querySelector('.btn').addEventListener('click', save_options);
