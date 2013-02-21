82f469979c7ce48066f06a96175368468fb811da
agate <agate.hao@gmail.com>
2013-02-21 10:22:20 +0800

    implemented selector

--- contentscript.js
+++ contentscript.js
@@ -11,3 +11,20 @@ function runBook() {
 function stopBook() {
   alert("bookStop");
 }
+
+$("body").append("<div id=\"scrapbook\">css selector: <input id=\"scrapbook-selector\"/></div><div class=\"scrapbook-highlight\"/>");
+
+var $cssSelector = $("#scrapbook-selector");
+var $highlighter = $(".scrapbook-highlight");
+
+$cssSelector.keyup(function () {
+  var $selector = $($cssSelector.val()).eq(0);
+  var offset = $selector.offset();
+
+  console.log($cssSelector.val(), $selector, offset);
+
+  $highlighter.css("top", offset.top);
+  $highlighter.css("left", offset.left);
+  $highlighter.width($selector.width());
+  $highlighter.height($selector.height());
+});
