726b09e2bc0748aa9aed76f0e02a7c770012aee7
agate <agate.hao@gmail.com>
2013-02-22 23:32:01 +0800

    implemented api submit

--- build/scrapbook/factual_api.js
+++ build/scrapbook/factual_api.js
@@ -5,15 +5,16 @@ $m.Class.extend("Scrapbook.FactualApi", function(KLASS, OO){
 
   OO.addMember("submit", function(viewId, values, callback){var self=this;
     var url = BASE_URL + "/t/" + viewId + "/submit";
+    var data = {
+      values: JSON.stringify(values),
+      user: "Scrapbook"
+    };
 
-    this.getAuthorization("POST", url, function (authorization) {
+    this.getAuthorization("POST", url, data, function (authorization) {
       $.ajax({
         url: url,
         type: "POST",
-        data: {
-          values: JSON.stringify(values),
-          user: "Scrapbook"
-        },
+        data: data,
         beforeSend: function(xhr) {
           xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
           xhr.setRequestHeader('Authorization', authorization);
@@ -23,27 +24,14 @@ $m.Class.extend("Scrapbook.FactualApi", function(KLASS, OO){
         }
       });
     });
-
-    this.getAuthorization("GET", BASE_URL + "/t/places", function (authorization) {
-      $.ajax({
-        url: BASE_URL + "/t/places",
-        type: "GET",
-        beforeSend: function(xhr) {
-          xhr.setRequestHeader('Authorization', authorization);
-        },
-        success: function(res) {
-          callback(res);
-        }
-      });
-    });
   });
 
-  OO.addMember("getAuthorization", function(method, url, cb){var self=this;
+  OO.addMember("getAuthorization", function(method, url, params, cb){var self=this;
     chrome.storage.local.get("options", function (data) {
       var key = data["options"]["key"] || "";
       var secret = data["options"]["secret"] || "";
 
-      cb(Scrapbook.utils.oauthHeader(key, secret, method, url));
+      cb(Scrapbook.utils.oauthHeader(key, secret, method, url, params));
     });
   });
 });
--- build/utils.js
+++ build/utils.js
@@ -28,30 +28,18 @@ jQuery.fn.getPath = function (smart) {
 };
 
 $m.Class.extend("Scrapbook.Utils", function(KLASS, OO){
-  OO.addMember("oauthHeader", function(ck, cks, method, encodedurl){var self=this;
-    var accessor = { consumerSecret: cks, tokenSecret: ""};
-    var message = { action: encodedurl, method: method.toUpperCase(), parameters: [["oauth_version","1.0"],["oauth_consumer_key",ck]]};
+  OO.addMember("oauthHeader", function(ck, cks, method, url, params){var self=this;
+    params = $.extend(true, {
+      "oauth_version": "1.0",
+      "oauth_consumer_key": ck
+    }, params || {});
 
-    OAuth.setTimestampAndNonce(message);
-    OAuth.SignatureMethod.sign(message, accessor);
+    var accessor = { consumerKey: ck, consumerSecret: cks };
+    var message = { action: url, method: method.toUpperCase(), parameters: params};
 
-    var parameters = OAuth.getParameterMap(message).parameters;
-    parameters = parameters.sort(function(a, b){
-      if (a[0] < b[0]) return -1;
-      if (a[0] == b[0]) return 0;
-      if (a[0] > b[0]) return 1;
-     });
+    OAuth.completeRequest(message, accessor);
 
-    var parameterMap = OAuth.getParameterMap(message);
-    var baseStr = OAuth.decodeForm(OAuth.SignatureMethod.getBaseString(message));
-    var theSig = "";
-
-    var authorization = [];
-    parameters.forEach(function($1,$2,$3){
-      authorization.push($1[0] + "=\"" + $1[1] + "\"");
-    });
-
-    return "OAuth " + authorization.join(", ");
+    return OAuth.getAuthorizationHeader(null, message.parameters);
   });
 });
 
